00254e5e16eb09783b9ab2628a87b7d966f03503
yutaka.miyazaki <mono.12dev@gmail.com>
2019-11-02 11:59:10 +0900

    fix

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { useStaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 
 interface SEOProps {
   description?: string;
@@ -9,68 +9,73 @@ interface SEOProps {
   title: string;
 }
 
-const Seo = ({ description, lang = 'ja', meta, title }: SEOProps) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            url
-          }
-        }
-      }
-    `
-  );
-
-  const metaDescription = description || site.siteMetadata.description;
-  const defaultTitle = "Pigmon.io | Yutazon's room";
-
+const SEO = ({ description, lang = 'ja', meta, title }: SEOProps) => {
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
+    <StaticQuery
+      query={detailsQuery}
+      render={data => {
+        const metaDescription = description || data.site.siteMetadata.description;
+        const defaultTitle = "Pigmon.io | Yutazon's room";
+        return (
+          <Helmet
+            htmlAttributes={{
+              lang,
+            }}
+            defaultTitle={defaultTitle}
+            title={title}
+            titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+            meta={[
+              {
+                name: `description`,
+                content: metaDescription,
+              },
+              {
+                property: `og:title`,
+                content: title,
+              },
+              {
+                property: `og:description`,
+                content: metaDescription,
+              },
+              {
+                property: `og:type`,
+                content: `website`,
+              },
+              {
+                name: `twitter:card`,
+                content: `summary`,
+              },
+              {
+                name: `twitter:creator`,
+                content: data.site.siteMetadata.author,
+              },
+              {
+                name: `twitter:title`,
+                content: title,
+              },
+              {
+                name: `twitter:description`,
+                content: metaDescription,
+              },
+            ].concat(meta || [])}
+          />
+        );
       }}
-      defaultTitle={defaultTitle}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta || [])}
     />
   );
 };
 
-export default Seo;
+const detailsQuery = graphql`
+  query SeoQuery {
+    site {
+      siteMetadata {
+        title
+        author
+        description
+        siteUrl
+      }
+    }
+  }
+`;
+
+export default SEO;
